278807b81920de8ed5ce039234f21de27dded3fb
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-29 19:59:54 -0800

    get user input

--- src/commands/react/react.action.ts
+++ src/commands/react/react.action.ts
@@ -1,3 +1,45 @@
-import { ReactOptions } from '../../types.js';
+import chalk from 'chalk';
+import { confirm, select } from '@inquirer/prompts';
+import { strings } from '../../strings.js';
 
-export function handleReactTemplates(appName: string, options: ReactOptions) {}
+export async function handleReactTemplates(appName: string) {
+  console.log(chalk.white('App Name:'), chalk.blue(appName));
+  const { usingMui, language } = await getInfo();
+  console.log(chalk.white(usingMui));
+  console.log(chalk.white(language));
+}
+
+async function getInfo() {
+  const usingMui = await getUsingMui();
+  const language = await getLanguage();
+
+  return { usingMui, language };
+}
+
+async function getUsingMui() {
+  try {
+    return await confirm({ message: strings.react.confirmMui, default: true });
+  } catch (err) {
+    return;
+  }
+}
+
+async function getLanguage() {
+  try {
+    return await select({
+      message: strings.react.selectLanguage,
+      choices: [
+        {
+          name: 'TS',
+          value: 'ts',
+        },
+        {
+          name: 'JS',
+          value: 'js',
+        },
+      ],
+    });
+  } catch (err) {
+    return;
+  }
+}
--- src/commands/react/react.command.ts
+++ src/commands/react/react.command.ts
@@ -1,16 +1,13 @@
 import { Command } from 'commander';
-import { ReactOptions } from '../../types.js';
 import { handleReactTemplates } from './react.action.js';
 
 const react = new Command('react');
 react
   .description('Creates react-ts template using vite with React MUI')
   .argument('<name>', 'name of your application')
-  .option('--no-mui,', 'add MUI library', false) // with --mui, -m will generate option name of mui as well
-  .option('--js', 'Use JS intead of TS', false)
-  .action((appName: string, options: ReactOptions) => {
+  .action((appName: string) => {
     // callback(argument, options) note that option names will be camel cased
-    handleReactTemplates(appName, options);
+    handleReactTemplates(appName);
   });
 
 export default react;
