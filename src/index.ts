73855e220d8a29cb735927b5261c55d88b9f6885
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-29 17:56:03 -0800

    add react command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import figlet from 'figlet';
 import { Command } from 'commander';
 import { strings } from './strings.js';
+import react from './commands/react.js';
 
 const program = new Command();
 
@@ -9,6 +10,8 @@ console.log(figlet.textSync(strings.title));
 program
   .version('1.0.0', '-v, --version', 'Get the current Scaffold version')
   .description('Personal CLI for creating project templates');
+
+program.addCommand(react);
 
 program.parse(process.argv);
 
