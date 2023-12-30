5b072902fdd9e28365863d36a36fe8c03b13e1e9
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-29 20:11:51 -0800

    change app name colour

--- src/commands/react/react.action.ts
+++ src/commands/react/react.action.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import { getInfo } from './react.input.js';
 
 export async function handleReactTemplates(appName: string) {
-  console.log(chalk.white('App Name:'), chalk.blue(appName));
+  console.log(chalk.white('App Name:'), chalk.cyan(appName));
   const { usingMui, language } = await getInfo();
   console.log(chalk.white(usingMui));
   console.log(chalk.white(language));
