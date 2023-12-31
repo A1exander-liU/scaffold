302c2f4bce569921704f0fe2562146457a1c124a
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-30 19:19:47 -0800

    reformat

--- src/templates/react-ts-base/vite.config.ts
+++ src/templates/react-ts-base/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      "process.env.TOKEN": JSON.stringify(env.TOKEN),
+      'process.env.TOKEN': JSON.stringify(env.TOKEN),
     },
     plugins: [react()],
   };
