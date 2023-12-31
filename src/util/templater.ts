b108200dcd7d9ec32c51c01233bc3e3b09aefb04
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-30 17:36:09 -0800

    make generateMuiFiles

--- src/util/templater.ts
+++ src/util/templater.ts
@@ -5,10 +5,27 @@ import { execWithPromise } from './wrappers.js';
 
 export const excluded = ['node_modules'];
 
+function templatePath() {
+  const current = import.meta.url.replace(/^file:\/\/\//, '');
+  const templatePath = path.join(current, '../../../src/templates');
+  return templatePath;
+}
+
 export async function generateTemplate(template: string, dest: string) {
   const current = import.meta.url.replace(/^file:\/\/\//, '');
   const templatePath = path.join(current, '../../../src/templates', template);
   await recursiveCopy(templatePath, dest, { excluded });
+}
+
+export async function generateMuiFiles(dest: string) {
+  const template = templatePath();
+  const appFile = path.join(template, 'mui/App.tsx');
+  const mainFile = path.join(template, 'mui/main.tsx');
+  const topbarFile = path.join(template, 'mui/topbar.tsx');
+
+  recursiveCopy(appFile, path.join(dest, 'src', 'App.tsx'));
+  recursiveCopy(mainFile, path.join(dest, 'src', 'main.tsx'));
+  recursiveCopy(topbarFile, path.join(dest, 'src', 'components', 'topbar.tsx'));
 }
 
 export async function initializeGit(appName: string) {
