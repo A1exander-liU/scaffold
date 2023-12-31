1689a0a43a53572edae660f8df5f4e9fa11f46a1
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-30 16:59:41 -0800

    add conditional tasks for mui

--- src/commands/react/react.action.ts
+++ src/commands/react/react.action.ts
@@ -1,22 +1,21 @@
 import chalk from 'chalk';
 import { getInfo } from './react.input.js';
 import { execWithPromise } from '../../util/wrappers.js';
-import task from 'tasuku';
+import task, { Task } from 'tasuku';
 import path from 'path';
 import { generateTemplate, initializeGit } from '../../util/templater.js';
+import figlet from 'figlet';
 
 export async function handleReactTemplates(appName: string) {
   console.log(chalk.white('App Name:'), chalk.cyan(appName));
   const { usingMui, language } = await getInfo();
-  if (!usingMui) {
-    return;
-  }
+
+  const destination = path.join(process.cwd(), appName);
 
   task(
     `Scaffolding project in ${path.join(process.cwd(), appName)}`,
     async ({ task }) => {
       await task(`Generating initial template files`, async () => {
-        const destination = path.join(process.cwd(), appName);
         await generateTemplate(`react-${language}-base`, destination);
       });
 
@@ -25,15 +24,17 @@ export async function handleReactTemplates(appName: string) {
       });
 
       if (usingMui) {
-        await task('Installing React MUI', async () => {
-          await setupMui(appName);
+        await task('Setting up React MUI', async ({ task }) => {
+          await task('Installing dependencies', async () => {
+            await installMui(appName);
+          });
         });
       }
     },
   );
 }
 
-async function setupMui(appName: string) {
+async function installMui(appName: string) {
   await execWithPromise(
     `cd ${appName} && npm install @mui/material @emotion/react @emotion/styled @mui/icons-material`,
   );
